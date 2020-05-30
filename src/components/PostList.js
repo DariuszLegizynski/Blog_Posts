302e3dc3121de4ea1ac4587f50e6c5d7debb82ea
DariuszLegizynski <d.legizynski@gmail.com>
2020-05-30 22:41:41 +0200

    made a new action creator and hooked it up.

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { fetchPosts } from "../actions";
 
 const PostList = () => {
-	return <div>Post List</div>;
+	const [ posts, getPosts ] = useState("");
+
+	useEffect(() => {
+		getPosts(fetchPosts());
+	}, []);
+
+	return <div>Post List {posts.length}</div>;
 };
 
 export default PostList;
